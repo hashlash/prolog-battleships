c98861ddbb4ab77ad880146d09e7e056d8e1053e
hashlash <muh.ashlah@gmail.com>
2020-05-10 10:40:19 +0700

    reorganize

--- battleship.pl
+++ battleship.pl
@@ -4,12 +4,19 @@ battleship(Ships, Nrow, Ncol, Rows) :-
 	same_length(Nrow, Rows), maplist(same_length(Ncol), Rows),
 	append(Rows, Vs), Vs ins 0..1,
 	transpose(Rows, Cols),
+	% row and col ship segment
 	maplist(sum_cons, Rows, Nrow),
 	maplist(sum_cons, Cols, Ncol),
+	% number of ships constraint
 	sum(Ships, #=, Nmul), length(Omul, Nmul),
 		maplist(=(1), Omul), append(Omul, Ships, Sadv),
 	lines(Rows, Crow), lines(Cols, Ccol),
 	append(Crow, Ccol, C), permutation(Sadv, C).
+
+sum_cons(L, S) :- sum(L, #=, S).
+
+lines([], []).
+lines([H|T], C) :- lines(T, Ct), line(H, Ch), append(Ct, Ch, C).
 
 line([], []).
 line([1], [1]).
@@ -17,16 +24,11 @@ line([0|T], A) :- line(T, A).
 line([1,0|T], [1|B]) :- line(T, B).
 line([1,1|T], [A|B]) :- line([1|T], [At|B]), A #= At+1.
 
-lines([], []).
-lines([H|T], C) :- lines(T, Ct), line(H, Ch), append(Ct, Ch, C).
-
-sum_cons(L, S) :- sum(L, #=, S).
-
 problem(1,
         [1,2,3],
         [1,1,2,2],
         [3,0,1,2],
-        [[0,0,1,0],
-         [1,0,0,_],
+        [[_,_,_,_],
          [_,_,_,_],
-         [_,_,0,1]]).
+         [_,_,_,_],
+         [_,_,_,_]]).
