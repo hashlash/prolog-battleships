6055b0c33e5efbbaf77d17c0c3270065bd67c655
Muhammad Ridho Ananda <muhammad.ridho73@ui.ac.id>
2020-05-16 01:00:48 +0800

    add modular function

--- battleship.py
+++ battleship.py
@@ -4,14 +4,25 @@ from pprint import pprint
 
 prolog = Prolog()
 prolog.consult("battleship.pl")
-result = list(prolog.query(
-    "problem(1, Ships, Nrow, Ncol, Rows), \
-    setof(Rows, battleship(Ships, Nrow, Ncol, Rows), List)"
-    ))
 
-if result:    
-    result = result[0]
-    for answer in result["List"]:
-        pprint(answer)
-else:
-    print("No solution")
+def solve(Ships, Nrow, Ncol, Rows):
+    return list(prolog.query(
+            "problem(1, {}, {}, {}, {}), setof(Rows, battleship({}, {}, {}, {}), List)".format(
+                Ships, Nrow, Ncol, Rows,
+                Ships, Nrow, Ncol, Rows
+            )
+    ))
+    return result[0]
+
+
+if __name__ == "__main__":
+    result = list(prolog.query(
+        "problem(1, Ships, Nrow, Ncol, Rows), \
+        setof(Rows, battleship(Ships, Nrow, Ncol, Rows), List)"
+    ))
+    if result:    
+        result = result[0]
+        for answer in result["List"]:
+            pprint(answer)
+    else:
+        print("No solution")
