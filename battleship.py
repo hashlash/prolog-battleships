b3a21cb1fcb29bd3b78171f6b40b654a58ceeeec
hashlash <muh.ashlah@gmail.com>
2020-05-16 21:37:23 +0700

    remove example in main, recreate on tests module later

--- battleship.py
+++ battleship.py
@@ -20,16 +20,3 @@ def grid_none_to_prologany(grid):
 def solve(ships, row_clues, col_clues, grid):
     query = f'setof(Rows, battleship({ships}, {row_clues}, {col_clues}, {grid}), List)'
     return prolog.query(query)
-
-
-if __name__ == '__main__':
-    result = prolog.query(
-        'problem(2, Ships, Nrow, Ncol, Rows), '
-        'setof(Rows, battleship(Ships, Nrow, Ncol, Rows), List)'
-    )
-    if result:
-        result = next(result)
-        for answer in result['List']:
-            pprint(answer)
-    else:
-        print('No solution')
