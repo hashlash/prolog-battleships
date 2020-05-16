fbdd18d185bc11c0159458c216c3768989f4911c
hashlash <muh.ashlah@gmail.com>
2020-05-16 18:49:44 +0700

    add data preparation for api calls (array of boolean representation of the puzzle)

--- static/battleships.js
+++ static/battleships.js
@@ -45,14 +45,91 @@ Battleships.prototype.setShipCounts = function(shipCounts) {
     this.shipCounts = shipCounts
 }
 
+Battleships.prototype.getBoolPuzzle = function() {
+    var boolPuzzle = this.puzzleGrid.map(row => row.map(x => {
+            switch (x) {
+                case this.SHIP_PIECE_UNKNOWN: return null;
+                case this.SHIP_PIECE_EMPTY: return false;
+                case this.SHIP_PIECE_IMPLIED: return true;
+                default: return x;
+            }
+        })),
+        force = (i, j, val) => {
+            switch (boolPuzzle[i][j]) {
+                case null:
+                    boolPuzzle[i][j] = val;
+                    break;
+                case false:
+                    if (val !== false)
+                        throw `Conflicted boolean value on row: ${i} and col: ${j}`;
+                    boolPuzzle[i][j] = val;
+                    break;
+                case true:
+                    if (val !== true)
+                        throw `Conflicted boolean value on row: ${i} and col: ${j}`;
+                    boolPuzzle[i][j] = val;
+                    break;
+                default:
+                    if (val === false)
+                        throw `Conflicted boolean value on row: ${i} and col: ${j}`;
+            }
+        },
+        isInside = (i, j) => (0 <= i && i < this.nrOfRows && 0 <= j && j < this.nrOfColumns);
+
+    boolPuzzle.forEach((row, i) => row.forEach((x, j) => {
+        switch (x) {
+            case this.SHIP_PIECE_SUBMARINE:
+                boolPuzzle[i][j] = true;
+                if (isInside(i-1, j)) force(i-1, j, false);
+                if (isInside(i+1, j)) force(i+1, j, false);
+                if (isInside(i, j-1)) force(i, j-1, false);
+                if (isInside(i, j+1)) force(i, j+1, false);
+                break;
+            case this.SHIP_PIECE_END_UP:
+                boolPuzzle[i][j] = true;
+                if (isInside(i-1, j)) force(i-1, j, false);
+                if (isInside(i+1, j)) force(i+1, j, true);
+                break;
+            case this.SHIP_PIECE_END_RIGHT:
+                boolPuzzle[i][j] = true;
+                if (isInside(i, j-1)) force(i, j-1, true);
+                if (isInside(i, j+1)) force(i, j+1, false);
+                break;
+            case this.SHIP_PIECE_END_DOWN:
+                boolPuzzle[i][j] = true;
+                if (isInside(i-1, j)) force(i-1, j, true);
+                if (isInside(i+1, j)) force(i+1, j, false);
+                break;
+            case this.SHIP_PIECE_END_LEFT:
+                boolPuzzle[i][j] = true;
+                if (isInside(i, j-1)) force(i, j-1, false);
+                if (isInside(i, j+1)) force(i, j+1, true);
+                break;
+            case this.SHIP_PIECE_MIDSECTION_HORIZONTAL:
+                boolPuzzle[i][j] = true;
+                if (isInside(i, j-1)) force(i, j-1, true);
+                if (isInside(i, j+1)) force(i, j+1, true);
+                break;
+            case this.SHIP_PIECE_MIDSECTION_VERTICAL:
+                boolPuzzle[i][j] = true;
+                if (isInside(i-1, j)) force(i-1, j, true);
+                if (isInside(i+1, j)) force(i+1, j, true);
+                break;
+        }
+    }));
+
+    return boolPuzzle;
+}
+
 function BattleshipsManager(battleships) {
     this.battleships = battleships,
     this.cellWidth = 50,
     this.cellHeight = 50,
     this.horizontalClueDOM = document.getElementById('horizontal-clues'),
     this.verticalClueDOM = document.getElementById('vertical-clues'),
-    this.shipsDOM = document.getElementById('ships-list')
-    this.boardDOM = document.getElementById('puzzle-board')
+    this.shipsDOM = document.getElementById('ships-list'),
+    this.boardDOM = document.getElementById('puzzle-board'),
+    this.solveButtonDOM = document.getElementById('solve-button')
 }
 
 BattleshipsManager.prototype.initialize = function() {
@@ -90,5 +167,18 @@ BattleshipsManager.prototype.initialize = function() {
     this.boardDOM.style.gridTemplateColumns = `repeat(${this.battleships.getNrOfColumns()}, ${this.cellWidth}px)`,
     this.boardDOM.style.gridTemplateRows = `repeat(${this.battleships.getNrOfRows()}, ${this.cellHeight}px)`,
     this.shipsDOM.value = this.battleships.getShipCounts().join(' '),
-    this.shipsDOM.addEventListener('change', e => this.battleships.setShipCounts(e.target.value.split(' ').map(Number)))
+    this.shipsDOM.addEventListener('change', e => this.battleships.setShipCounts(e.target.value.split(' ').map(Number))),
+    this.solveButtonDOM.addEventListener('click', () => this.getSolution())
+}
+
+BattleshipsManager.prototype.getSolution = function() {
+    var puzzleJSON = {
+        rows: this.battleships.getNrOfRows(),
+        cols: this.battleships.getNrOfColumns(),
+        ships: this.battleships.getShipCounts(),
+        rowClues: this.battleships.getVerticalClues(),
+        colClues: this.battleships.getHorizontalClues(),
+        grid: this.battleships.getBoolPuzzle(),
+    };
+    console.log(puzzleJSON);
 }
