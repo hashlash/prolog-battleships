c94d94323f8a370bf59f875c71693226d9ae21c5
hashlash <muh.ashlah@gmail.com>
2020-05-16 21:36:37 +0700

    add prolog any for python None object

--- battleship.py
+++ battleship.py
@@ -5,6 +5,18 @@ prolog = Prolog()
 prolog.consult('battleship.pl')
 
 
+class PrologAny:
+    def __repr__(self):
+        return '_'
+
+    def __str__(self):
+        return '_'
+
+
+def grid_none_to_prologany(grid):
+    return [[PrologAny() if x is None else x for x in l] for l in grid]
+
+
 def solve(ships, row_clues, col_clues, grid):
     query = f'setof(Rows, battleship({ships}, {row_clues}, {col_clues}, {grid}), List)'
     return prolog.query(query)
