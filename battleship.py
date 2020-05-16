e7ce78b4772277a39753359268eefd9cbc636a14
hashlash <muh.ashlah@gmail.com>
2020-05-16 20:43:16 +0700

    fix styling

--- battleship.py
+++ battleship.py
@@ -1,25 +1,23 @@
 from pyswip.prolog import Prolog
 from pprint import pprint
 
-
 prolog = Prolog()
-prolog.consult("battleship.pl")
-
-def solve(Ships, Nrow, Ncol, Rows):
-    return prolog.query(
-            "setof(Rows, battleship({}, {}, {}, {}), List)".format(
-                Ships, Nrow, Ncol, Rows
-            ))
+prolog.consult('battleship.pl')
 
 
-if __name__ == "__main__":
+def solve(ships, row_clues, col_clues, grid):
+    query = f'setof(Rows, battleship({ships}, {row_clues}, {col_clues}, {grid}), List)'
+    return prolog.query(query)
+
+
+if __name__ == '__main__':
     result = prolog.query(
-        "problem(2, Ships, Nrow, Ncol, Rows), \
-        setof(Rows, battleship(Ships, Nrow, Ncol, Rows), List)"
+        'problem(2, Ships, Nrow, Ncol, Rows), '
+        'setof(Rows, battleship(Ships, Nrow, Ncol, Rows), List)'
     )
-    if result:    
+    if result:
         result = next(result)
-        for answer in result["List"]:
+        for answer in result['List']:
             pprint(answer)
     else:
-        print("No solution")
+        print('No solution')
